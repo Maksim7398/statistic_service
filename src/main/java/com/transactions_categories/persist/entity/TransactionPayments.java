a44ec282aad167380608512dbe0867682e13bc18
Maksim7398 <arzamasov.maksim1998@yandex.ru>
2024-05-14 11:04:37 +0300

    create table transaction-payments

---
+++ src/main/java/com/transactions_categories/persist/entity/TransactionPayments.java
@@ -0,0 +1,45 @@
+package com.transactions_categories.persist.entity;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.math.BigDecimal;
+import java.util.UUID;
+
+@Entity
+@Table(name = "transaction_payments")
+@Getter
+@Setter
+@NoArgsConstructor
+public class TransactionPayments {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
+    private UUID id;
+
+    private UUID userIdSource;
+
+    private UUID accountIdSource;
+
+    @Column(name = "total_sum")
+    private BigDecimal totalSum;
+
+    private String category;
+
+    private UUID userIdDestination;
+
+    private UUID accountIdDestination;
+
+    @Column(name = "bik_destination")
+    private Integer bikDestination;
+
+    @Column(name = "internalTransfer")
+    private Boolean internalTransfer;
+}
