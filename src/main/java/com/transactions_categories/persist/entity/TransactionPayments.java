f6eda79fbd8b6a32e1ee0abe3389f549b117c368
Maksim7398 <arzamasov.maksim1998@yandex.ru>
2024-05-14 13:48:17 +0300

    add column create_date

--- src/main/java/com/transactions_categories/persist/entity/TransactionPayments.java
+++ src/main/java/com/transactions_categories/persist/entity/TransactionPayments.java
@@ -11,6 +11,7 @@ import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 import java.math.BigDecimal;
+import java.time.LocalDate;
 import java.util.UUID;
 
 @Entity
@@ -42,4 +43,7 @@ public class TransactionPayments {
 
     @Column(name = "internalTransfer")
     private Boolean internalTransfer;
+
+    @Column(name = "create_date")
+    private LocalDate createDate;
 }
